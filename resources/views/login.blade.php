cbd447095ee260b5b10f484bb6f693ef7a35e991
hidasyaqiib <hidaps2008@gmail.com>
2024-10-14 15:26:20 +0700

    add index dan login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form id="loginForm">
+        <div>
+            <label for="username">Username : </label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="password">Password : </label>
+            <input type="text" name="password" id="password">
+        </div>
+        <button type="submit">LOGIN</button>
+    </form>
+
+    <script src="/assets/js/script.js"></script>
+    <link rel="stylesheet" href="/assets/css/style-login.css">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //merujuk kehalaman welcome.blade.php di folder resource/views
+});
+
+Route::get('/index', function () {
+    return view('index'); //merujuk kehalaman welcome.blade.php di folder resource/views
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
